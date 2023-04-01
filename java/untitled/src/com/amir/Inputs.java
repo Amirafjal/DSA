65d8998f23be57f98b54136e73cdc267f4975af4
Amirafjal <85572222+Amirafjal@users.noreply.github.com>
2023-04-01 20:02:53 +0530

    Inputs

---
+++ java/untitled/src/com/amir/Inputs.java
@@ -0,0 +1,24 @@
+package com.amir;
+
+import java.util.Scanner;
+
+public class Inputs     {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+//        System.out.println("What is your mobile no: ");
+//        long mobileNo = input.nextLong();
+//        System.out.println("Your mobile no is "+ mobileNo);
+//
+        int a = 234_000_000;
+        // a is called reference variable or identifier
+        // and 234_000_000 is called as literals.
+        System.out.println(a);
+
+//        String name = input.nextLine();
+//        System.out.println(name);
+
+        float marks = input.nextFloat();
+        System.out.println(marks);
+
+    }
+}
