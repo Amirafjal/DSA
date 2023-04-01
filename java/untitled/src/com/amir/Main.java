7b57659a97b400ed09dc0974a49cfaf8aab409b7
Amirafjal <85572222+Amirafjal@users.noreply.github.com>
2023-04-01 19:35:16 +0530

    primitives

--- java/untitled/src/com/amir/Main.java
+++ java/untitled/src/com/amir/Main.java
@@ -1,7 +1,11 @@
 package com.amir;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        Scanner input = new Scanner(System.in);
+        System.out.println(input.nextLine());
     }
 }
---
+++ java/untitled/src/com/amir/Primitives.java
@@ -0,0 +1,13 @@
+package com.amir;
+
+public class Primitives {
+    public static void main(String[] args) {
+        int rollno = 11; //4 byte
+        char letter = 'a';
+        float marks = 98.67f; //4 bytes
+        double largeDecimalNumbers = 5342334.323525; //8 byte
+        long largeInteger = 9809315809843109L; //8 byte
+        boolean check = false;
+
+    }
+}
