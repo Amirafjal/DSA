10b59f0b99f475a71ed17fa5de4eb7127764d17e
Amirafjal <85572222+Amirafjal@users.noreply.github.com>
2023-04-02 16:10:07 +0530

    BasicsLoop

---
+++ java/untitled/src/com/amir/Basics.java
@@ -0,0 +1,20 @@
+package com.amir;
+
+public class Basics     {
+    public static void main(String[] args) {
+        int a = 11;
+        if ( a == 11){
+            System.out.println("Hello world");
+        }
+
+        int count = 1;
+        while ( count != 5){
+            System.out.println(count);
+            count++;
+        }
+        //for loop
+        for(int c =1 ; c !=5; c++){
+            System.out.println(c);
+        }
+    }
+}
