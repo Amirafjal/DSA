fa92b7176107175606b97c5229f2b993f2bdf2d1
Amirafjal <85572222+Amirafjal@users.noreply.github.com>
2023-04-03 21:52:19 +0530

    for

---
+++ java/untitled/src/com/amir/Loops.java
@@ -0,0 +1,21 @@
+package com.amir;
+
+import java.util.Scanner;
+
+public class Loops {
+    public static void main(String[] args) {
+
+        for (int num = 0; num <= 5; num+=1) {
+            System.out.println(num);
+        }
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+
+        for (int i = 0; i <= n ; i++) {
+          //  System.out.print(i +" ");
+            System.out.println("amir");
+        }
+        //while loops
+
+    }
+}
