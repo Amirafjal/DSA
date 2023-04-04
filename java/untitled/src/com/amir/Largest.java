ec35bea29fcf2e9ea1affbefdb76c7641030f500
Amirafjal <85572222+Amirafjal@users.noreply.github.com>
2023-04-04 23:04:52 +0530

    Largest of 3 num

---
+++ java/untitled/src/com/amir/Largest.java
@@ -0,0 +1,22 @@
+package com.amir;
+
+import java.util.Scanner;
+
+public class Largest {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+
+        int max = a;
+        if (b > max){
+            max = b;
+        }
+        if (c > max){
+            max = c;
+        }
+        System.out.println(max);
+
+    }
+}
