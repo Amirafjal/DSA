b393238bb34a1fe01e7f46889faf8a8b5be1f974
Amirafjal <85572222+Amirafjal@users.noreply.github.com>
2023-04-05 15:28:42 +0530

    Largest of 3 num

--- java/untitled/src/com/amir/Largest.java
+++ java/untitled/src/com/amir/Largest.java
@@ -9,13 +9,27 @@ public class Largest {
         int b = sc.nextInt();
         int c = sc.nextInt();
 
-        int max = a;
-        if (b > max){
-            max = b;
-        }
-        if (c > max){
-            max = c;
-        }
+//        int max = a;
+//        if (b > max){
+//            max = b;
+//        }
+//        if (c > max){
+//            max = c;
+//        }
+
+//        int max =0;
+//        if (a > b){
+//            max = a;
+//        }else{
+//            max =b;
+//        }
+//        if (c > max){
+//            max = c;
+//        }
+
+        int  max = Math.max(c,Math.max(a,b));
+
+
         System.out.println(max);
 
     }
