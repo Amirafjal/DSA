65dbd800306e583b759930b6d997cb9206beceee
Amirafjal <85572222+Amirafjal@users.noreply.github.com>
2023-04-05 19:06:32 +0530

    CountNums

---
+++ java/untitled/src/com/amir/CountingOccurrences.java
@@ -0,0 +1,18 @@
+package com.amir;
+
+public class CountingOccurrences {
+    public static void main(String[] args) {
+
+        int n = 45536;
+        int count =0;
+
+        while(n > 0){
+            int remainder = n % 10;
+            if(remainder == 5){
+                count ++;
+            }
+            n = n / 10;
+        }
+        System.out.println(count);
+    }
+}
