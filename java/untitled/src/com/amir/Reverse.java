a1624db176d4c8a3378e2f29b91d4140b5bfdb3d
Amirafjal <85572222+Amirafjal@users.noreply.github.com>
2023-04-05 19:35:29 +0530

    Reverse

---
+++ java/untitled/src/com/amir/Reverse.java
@@ -0,0 +1,18 @@
+package com.amir;
+
+import java.util.Scanner;
+
+public class Reverse {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int reverse = 0;
+        while(n > 0){
+            int remainder = n % 10;
+            reverse = (reverse*10) + remainder;
+            n = n / 10;
+
+        }
+        System.out.println(reverse);
+    }
+}
