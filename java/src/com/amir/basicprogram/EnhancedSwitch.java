1b5fc1a20142be877ee2e53e9b535d4d000e4f3f
Amirafjal <85572222+Amirafjal@users.noreply.github.com>
2023-04-06 20:22:47 +0530

    EnhancedSwitch

--- java/src/com/amir/basicprogram/EnhancedSwitch.java
+++ java/src/com/amir/basicprogram/EnhancedSwitch.java
@@ -14,5 +14,15 @@ public class EnhancedSwitch {
             case "grape" -> System.out.println("small fruit");
             default -> System.out.println("Please Enter a valid fruit");
         }
+        int day = in.nextInt();
+        switch (day){
+            case 1 -> System.out.println("Monday");
+            case 2 -> System.out.println("Tuesday");
+            case 3 -> System.out.println("Wednesday");
+            case 4 -> System.out.println("Thursday");
+            case 5 -> System.out.println("Friday");
+            case 6 -> System.out.println("Saturday");
+            case 7 -> System.out.println("Sunday");
+        }
     }
 }
