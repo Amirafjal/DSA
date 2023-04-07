176f92a28d5ec6c3a09a5cf364b6365e6201dee1
Amirafjal <85572222+Amirafjal@users.noreply.github.com>
2023-04-07 16:19:08 +0530

    changeValueBy Fn.

---
+++ java/src/com/amir/functions_or_methods/Swap_04.java
@@ -0,0 +1,25 @@
+package com.amir.functions_or_methods;
+
+public class Swap_04 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 30;
+
+//        int temp =a ;
+//        a = b;
+//        b = temp;
+//        System.out.println(a+ " "+b);
+
+        System.out.println("main method : "+a +" "+b);
+        swap(a,b);
+    }
+    static void swap(int num1 , int num2){
+
+        int temp = num1;
+        num1 = num2;
+        num2 = temp;
+
+        System.out.println("swap method : "+num1+ " " +num2);
+    }
+
+}
