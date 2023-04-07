290e16252808680c1cc79b1549c66de8171d3026
Amirafjal <85572222+Amirafjal@users.noreply.github.com>
2023-04-07 17:29:16 +0530

    scopping in block

--- java/src/com/amir/functions_or_methods/Scope.java
+++ java/src/com/amir/functions_or_methods/Scope.java
@@ -5,11 +5,25 @@ public class Scope {
 
         int a = 10;
         int b = 20;
-        System.out.println(marks);//wrong scope
-        System.out.println(num);//wrong scope
+        String name = "amir";
+        //System.out.println(marks);//wrong scope
+        //System.out.println(num);//wrong scope
 
         System.out.println(a);//correct scope
         System.out.println(b);//correct scope
+
+        //Scoping in Block=====
+        {
+            //int a = 7; //already initialised outside the block in the same method , hence you can not initialised again
+            a =100; //But you can change the value or we say Reassign the original ref variable to some other value
+            System.out.println(a);
+            name = "afjal";
+
+            int c = 99; //value initialised in this block , will remain in the block.
+        }
+        System.out.println(a);
+        System.out.println(name);//not created or initialised original new name so it will execute updated.
+       // System.out.println(c); Here created and initialised so not working // wrong because we can't use outside of block , it should be remain in the block to be executable.
 
     }
     static void random(int marks){
@@ -17,7 +31,7 @@ public class Scope {
         System.out.println(num);//correct scope
         System.out.println(marks);//correct scope
 
-        System.out.println(a);//wrong scopping
-        System.out.println(b);//wrong scope
+        //System.out.println(a);//wrong scopping
+        //System.out.println(b);//wrong scope
     }
 }
