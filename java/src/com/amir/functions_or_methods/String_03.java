53d0b313c9f71c29e1fa28e6803ecb1a1161e3d6
Amirafjal <85572222+Amirafjal@users.noreply.github.com>
2023-04-07 10:06:19 +0530

    String Functions/Methods

--- java/src/com/amir/functions_or_methods/String_03.java
+++ java/src/com/amir/functions_or_methods/String_03.java
@@ -3,5 +3,11 @@ package com.amir.functions_or_methods;
 public class String_03 {
     public static void main(String[] args) {
 
+        String message = greet();
+        System.out.println(message);
+    }
+    static String greet(){
+        String greeting = "HI, How are you?";
+        return greeting;
     }
 }
