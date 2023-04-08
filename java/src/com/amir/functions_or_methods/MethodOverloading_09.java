e768cc5836493ba0db574b496d3f99f8ee1b9a9a
Amirafjal <85572222+Amirafjal@users.noreply.github.com>
2023-04-08 21:57:57 +0530

    MethodOverloading

--- java/src/com/amir/functions_or_methods/MethodOverloading_09.java
+++ java/src/com/amir/functions_or_methods/MethodOverloading_09.java
@@ -1,10 +1,18 @@
 package com.amir.functions_or_methods;
 
-public class MethodOverloading {
+public class MethodOverloading_09 {
     public static void main(String[] args) {
 
         fun(69);
         fun("amir");
+        System.out.println(sum(3,5));
+        System.out.println(sum(7,8,9));
+    }
+    static int sum(int a, int b){
+        return a+b;
+    }
+    static int sum(int a, int b , int c){
+        return a+b+c;
     }
     static void fun(int a){
         System.out.println(a);
