0c454fd4c5864b3de46ffec2cd7a5d39e76f79d5
Amirafjal <85572222+Amirafjal@users.noreply.github.com>
2023-04-10 21:31:49 +0530

    Questions

---
+++ java/src/com/amir/functions_or_methods/Questions.java
@@ -0,0 +1,10 @@
+package com.amir.functions_or_methods;
+
+import java.util.Scanner;
+
+public class Questions {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+    }
+}
