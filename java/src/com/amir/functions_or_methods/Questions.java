60429f0a92ae03ade402195b80d73dc88632293a
Amirafjal <85572222+Amirafjal@users.noreply.github.com>
2023-04-10 21:33:54 +0530

    Questions

--- java/src/com/amir/functions_or_methods/Questions.java
+++ java/src/com/amir/functions_or_methods/Questions.java
@@ -5,6 +5,6 @@ import java.util.Scanner;
 public class Questions {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-
+        int n = in.nextInt();
     }
 }
